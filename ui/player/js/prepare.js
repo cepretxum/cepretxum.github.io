08968a0b640a13f328ddcd33f021410225809234
cepretxum <140847358+cepretxum@users.noreply.github.com>
2023-11-30 10:00:07 +0000

    Add files via upload

---
+++ ui/player/js/prepare.js
@@ -0,0 +1,18 @@
+
+function $(id){return document.getElementById(id);}
+
+
+
+
+video_data = {
+    url : "https://download1528.mediafire.com/o2ksyyd1aqagANF2k2nGUceMsZrCCmeySnzl7VFPRxvuQXuN4s0x-W5Ekp8vZ4DQ4EUr32ifCYevv-StEOjELS0SVoOco4IOhwBOoSAhqADaULL-S0pZC14rsrqlmr7ret2mJYMz_81S5zhUJV8VgkBydxvqchFOxcLxvZcX0wci_F83/twwci9c8va99ldg/Tag.2023.1080P.WEB-DL.AKWAM.mp4",
+    subtitle:""
+}
+
+
+
+$("video").src=video_data.url;
+$("subtitle").src=video_data.subtitle;
+
+
+
