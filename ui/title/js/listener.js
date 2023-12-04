8b8c0bb6d4f7b45e7d5d4db8d46c05aaea670c6c
cepretxum <140847358+cepretxum@users.noreply.github.com>
2023-12-04 22:10:22 +0000

    Add files via upload

--- ui/title/js/listener.js
+++ ui/title/js/listener.js
@@ -12,11 +12,29 @@ function cardListener(e){
 
 function playButtonListener(e){
 
+    /*
     player_url = window.location.origin+"/ui/player/player.html?imdbId="+params.get("imdbId");
 
     Tree.Player(player_url);
-    
+    */
+
+    netscape_url = window.location.origin+"/ui/player/netscape/netscape.txt";
+
+
+    v_id = "11ghnhcc8vIUKi6ZmiPVe62fsyRil2B7G";
+    v_uuid = "6388cd88-bbe6-4c5a-b6f9-fcf769818162";
+    v_at = "APZUnTVTSODr5F7lCVZCezYO9x_e:1701635377341";
+
+    video_url = build_drive_download_link(v_id,v_uuid,v_at);
+
+    Tree.CustomP3(video_url,netscape_url);
 }
+
+function build_drive_download_link(id,uuid,at){
+    b = `https://drive.usercontent.google.com/download?id=${id}`;
+    c = `&export=download&authuser=0&confirm=t&uuid=${uuid}&at=${at}`;
+    return b+c;
+    }
 
 
 
