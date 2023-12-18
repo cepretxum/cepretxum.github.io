3f5d9853631abac3b62bdb3f1332259f2e992b3a
cepretxum <140847358+cepretxum@users.noreply.github.com>
2023-12-18 19:29:34 +0000

    Add files via upload

--- ui/main/js/category.js
+++ ui/main/js/category.js
@@ -4,6 +4,63 @@ function $(id) { return document.getElementById(id); }
 let categories = document.getElementsByClassName("category");
 
 
+
+
+function draw_category_content(board_ref,cat_imdbIds){
+
+    for (var i in cat_imdbIds){
+
+
+        fetch(`/data/item/${cat_imdbIds[i]}.json`)
+             .then((resp)=>resp.json())
+             .then((data)=>{
+    
+              var div = document.createElement("div");
+              div.innerHTML = `<div class='card' id='${data.imdbId}'>
+              <div class="title_poster" onclick="cardListener(this)"><img src="/data/media/image/${data.imdbId}.jpg" width="100%" height="100%"></img> </div>
+              <div class="title_name" onclick="cardListener(this)">${data.name.slice(0, 20) + "..."}</div>
+              <div class="title_genre" id="title_genre"><div class="genre_box">${translate_list(data.genre).join("</div><div class='genre_box'>")}</div></div>
+    
+    
+              <div class="year_imdb">
+    
+              <div class="title_year">${data.year}</div>
+    
+              <div class="title_imdb">
+    
+    
+              <div class="title_imdb_star"> <div class="full-star"></div> </div>
+              <div class="title_imdb_rate"> ${data.rating}</div>
+              
+              </div>
+              
+              </div>
+    
+    
+              </div>`;
+              board_ref.appendChild(div);
+    
+             })
+    
+    
+    
+      }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 for (var i=0;i<categories.length;i++){
 
     
@@ -20,6 +77,8 @@ for (var i=0;i<categories.length;i++){
             console.log(cat_name);
             console.log(cat_board);
             console.log(cat_imdbIds);
+
+            draw_category_content(cat_board,cat_imdbIds);
 
          })
 
