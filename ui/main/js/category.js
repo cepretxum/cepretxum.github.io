664a307a44bbcb7fa698eb3012447462eb5e54e9
cepretxum <140847358+cepretxum@users.noreply.github.com>
2023-12-18 19:18:41 +0000

    Add files via upload

---
+++ ui/main/js/category.js
@@ -0,0 +1,31 @@
+
+function $(id) { return document.getElementById(id); }
+
+let categories = $("category");
+
+
+for (var i=0;i<categories.length;i++){
+
+    
+
+    cat_name = categories.item(i).id;
+    cat_board = categories.item(i).getElementsByClassName("category-content")[0];
+
+    fetch(`/data/items-map/category/${cat_name}.json`)
+         .then((resp)=>resp.json())
+         .then((data)=>{
+
+            cat_imdbIds = data;
+
+            console.log(cat_name);
+            console.log(cat_board);
+            console.log(cat_imdbIds);
+
+         })
+
+
+
+}
+
+
+
