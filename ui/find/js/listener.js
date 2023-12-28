f6300fc7034a5309d420632e83e4712c1a6ee2f4
cepretxum <140847358+cepretxum@users.noreply.github.com>
2023-12-28 01:47:49 +0000

    Add files via upload

--- ui/find/js/listener.js
+++ ui/find/js/listener.js
@@ -3,15 +3,15 @@
 
 /* events listener */
 
-
-
 function cardListener(e){
+
+    /*
     window.location.href="/ui/title/title.html?imdbId="+e.parentNode.id;
+    */
+
+    title_url = window.location.origin+"/ui/title/title.html?imdbId="+e.parentNode.id;
+    Tree.Title(title_url);
+
 }
-
-
-
-
-
 
 
