77d27b370f89283b3538ce32348a1218e31bde83
cepretxum <140847358+cepretxum@users.noreply.github.com>
2024-01-05 21:52:01 +0200

    Update listener.js

--- ui/title/js/listener.js
+++ ui/title/js/listener.js
@@ -22,36 +22,14 @@ function playButtonListener(e){
     Msg();
     Ads.showRewardedAd();
     setTimeout(function() {
-        //customp3_play("1yfRvES0uVPio2BZHcfpHoCLCxg6jomlC",0);
-        Ocustomp3_play("https://flawtantaedu-my.sharepoint.com/personal/ug_31146395_f-law_tanta_edu_eg/_layouts/15/download.aspx?UniqueId=f1bda0ec%2Dde29%2D471e%2Da3d3%2D48c843485940");
+        
+        customp3_play(params.get("imdbId"));
+        
     }, 1000);
     
 
-    /*
-    netscape_url = window.location.origin+"/ui/player/netscape/netscape.txt";
-    C = "";
-    if(Network.isConnected()){
-        C = Requests.GET(netscape_url);
-    }
-
-
-
-
-    v_id = "1CEpQy-GuxySOz9mssy74NCSozrSwXJ3S";
-    v_uuid = "8472c0e1-d073-445c-9c3b-dd6df8e3021a";
-    v_at = "APZUnTUawUyrLq7GpyidRbfoHti7:1701862330822";
-
-    video_url = build_drive_download_link(v_id,v_uuid,v_at);
-
-    Tree.CustomP3(video_url,netscape_url,C);
-   */
 }
 
-function build_drive_download_link(id,uuid,at){
-    b = `https://drive.usercontent.google.com/download?id=${id}`;
-    c = `&export=download&authuser=0&confirm=t&uuid=${uuid}&at=${at}`;
-    return b+c;
-    }
 
 
 
